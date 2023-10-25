c107eb0f1771b5a94e00a2618aecc737a5cac76d
26june <gj.abordo1@gmail.com>
2023-10-25 16:15:07 +0100

    add placeholder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,10 @@ export default function Header({ currentSearch, setCurrentSearch }) {
   return (
     <div className="Header">
       <h1>Horned Beasts</h1>
+
       <input
         type="text"
+        placeholder="Enter a keyword..."
         value={currentSearch}
         onChange={(e) => setCurrentSearch(e.target.value)}
       />
